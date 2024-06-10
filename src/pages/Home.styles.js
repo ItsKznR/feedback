543c80c353a1cb1cc249a2ccc63110584fe4eed5
Kauã  Ramos de Souza <kzndiscord@gmail.com>
2024-06-10 01:39:30 +0000

    style: estilização do h1 e p do  card

--- src/pages/Home.styles.js
+++ src/pages/Home.styles.js
@@ -1,9 +1,26 @@
 import styled from "styled-components"
+import { theme } from "../styles/theme";
 
 export const CardContainer = styled.div`
     background: ${props => props.theme.cardGradient};
     max-widht: 41.2rem;
     padding: 3.2rem;
+    border-radius: 3rem;
+    
+
+    h1 {
+    font-size: 2.8rem;
+    color: ${props => props.theme.white};
+    margin-bottom: 1.5rem;
+    text-align: center;
+    }
+
+    p {
+        font-size: 1.5rem;
+        line-heigh: 2.4rem;
+        color: ${props => props.theme.textColor};
+        text-align: center;
+    }
 `
 
 export const IconContainer = styled.div``
